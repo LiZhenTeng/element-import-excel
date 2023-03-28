6c6e8ebb03626c1d3cc99b69b80c63c66f115e01
Li ZhenTeng <2943560252@qq.com>
2023-03-28 13:02:54 +0800

    fix:css

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from "path";
-import esbuild from 'rollup-plugin-esbuild'
-import commonjs from '@rollup/plugin-commonjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -27,16 +25,7 @@ export default defineConfig({
         globals: {
           vue: "Vue"
         }
-      },
-      plugins: [
-        commonjs(),
-        esbuild({
-          sourceMap: true,
-          target: "es2018",
-          loaders: {
-            '.vue': 'ts',
-          }
-        })]
+      }
     },
     cssCodeSplit: true
   }
