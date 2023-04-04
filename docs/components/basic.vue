a22125791e2fbcba6edf5c6dfb811f4ec27ac76b
Li ZhenTeng <2943560252@qq.com>
2023-04-04 16:13:05 +0800

    feat:docs

--- docs/components/basic.vue
+++ docs/components/basic.vue
@@ -1,10 +1,8 @@
 <template>
-    <el-button @click="visible = true">Click</el-button>
     <ImportView :visible="visible" :fields="fields" :read-success="readSuccess" @close="visible = false" />
 </template>
 <script setup lang="ts">
 import { ref } from 'vue';
-import { ElButton } from 'element-plus';
 const visible = ref(false);
 const fields = ref({ name: '姓名' });
 const readSuccess = (data) => {
